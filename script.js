d6ec22e09f01cb0cf4f5a59a9006e234f31ca0e2
Sofia <hanziypetro@gmail.com>
2022-01-21 16:46:26 +0200

    Functiom

--- script.js
+++ script.js
@@ -1,108 +1,113 @@
 "use strict"
 
-const arr = [1,2,3,1,2];
+// ВИДАЛЯЄ ОДНАКОВІ З МАСИВУ
+// const arr = [1,2,3,1,2];
+
+// function deleteDuplicate(arr) {
+//    var newArray = [];
+//    arr.forEach((el, i) => {
+//       if (newArray.indexOf(el) === -1) {
+//          newArray.push(el);
+//       }
+//     });
+//     console.log(newArray);
+// };
+
+// deleteDuplicate(arr);
 
 
-function deleteDuplicate(arr) {
-   var newArray = [];
-   arr.forEach((el, i) => {
-      if (newArray.indexOf(el) === -1) {
-         newArray.push(el);
-      }
-    });
-    console.log(newArray);
-};
 
-deleteDuplicate(arr);
+// ВІДБИРАЄ ОДНАКОВІ З РІЗНИХ МАСИВІВ І ФОРМУЄ З НИХ НОВИЙ
+// const arrFirst = [1, 2];
+// const arrSecond = [2, 3];
 
-
-
-
-const arrFirst = [1, 2],
-      arrSecond = [2, 3];
-
-function arrayIntersection(oneArgument, twoArgument) {
-   const arrOne = new Set(oneArgument);
-   const arrTwo = new Set(twoArgument);
-   let newArray = [];
-   for (let i of arrTwo) {
+// function arrayIntersection(oneArgument, twoArgument) {
+//    const arrOne = new Set(oneArgument);
+//    const arrTwo = new Set(twoArgument);
+//    let newArray = [];
+//    for (let i of arrTwo) {
    
-       if (arrOne.has(i)) {
-         newArray.push(i);
-       }
+//        if (arrOne.has(i)) {
+//          newArray.push(i);
+//        }
        
-   }  
-   return newArray;
+//    }  
+//    return newArray;
 
-}
+// }
 
-const audit = arrayIntersection(arrFirst, arrSecond);
-console.log(audit);
+// const audit = arrayIntersection(arrFirst, arrSecond);
+// console.log(audit);
 
 
 
-const obj1 = {},
-      obj2 = { a: { b: undefined } },
-      obj3 = { a: { b: [] } };
+
+
+
+const obj1 = {};
+ const obj2 = { a: { b: undefined } };
+ const obj3 = { a: { b: [] } };
  
 
-const deepСheck = (obj) => {
+
+const isEmptyDeep = (obj) => {
+
    if (obj === null) {
      return true;
    } 
 
    if (Array.isArray(obj)) {
-
-      let entrance;
-      for (let i = 0; i < obj.length; i++) {
-         if (typeof obj[i] === 'boolean' || (typeof obj[i] === 'number' && !Number.isNaN(obj[i])) || (typeof obj[i] === 'string' && obj[i] !== '')) {
-               entrance = false;
-            break;
-         }
-         if (Array.isArray(obj[i]) || (typeof obj[i] === 'object' && obj[i] !== null)) {
-            entrance = deepСheck(obj[i]);
-            break;
-         }
-         entrance = true;
-      }
-
-      if (obj.length === 0) {
-         return true;
-      }
-
-      return entrance;
-   }
-
-   if (typeof obj === 'object') {
-     const objitems = Object.keys(obj);
-     if (objitems.length === 0) {
+     if (obj.length === 0) {
        return true;
      }
  
-     let entrance;
-     for (let i = 0; i < objitems.length; i++) {
-       const value = obj[objitems[i]];
+     let result;
+     for (let i = 0; i < obj.length; i += 1) {
+       if (typeof obj[i] === 'boolean' || (typeof obj[i] === 'number' && !Number.isNaN(obj[i]))
+         || (typeof obj[i] === 'string' && obj[i] !== '')) {
+         result = false;
+         break;
+       }
+       if (Array.isArray(obj[i]) || (typeof obj[i] === 'object' && obj[i] !== null)) {
+         result = isEmptyDeep(obj[i]);
+         break;
+       }
+ 
+       result = true;
+     }
+ 
+     return result;
+   }
+   if (typeof obj === 'object') {
+     const objectKeys = Object.keys(obj);
+     if (objectKeys.length === 0) {
+       return true;
+     }
+ 
+     let result;
+     for (let i = 0; i < objectKeys.length; i += 1) {
+       const value = obj[objectKeys[i]];
  
        if (typeof value === 'boolean' || (typeof value === 'number' && !Number.isNaN(value))
          || (typeof value === 'string' && value !== '')) {
-            entrance = false;
+         result = false;
          break;
        }
        if (Array.isArray(value) || (typeof value === 'object' && value !== null)) {
-         entrance = deepСheck(value);
+         result = isEmptyDeep(value);
          break;
        }
  
-       entrance = true;
+       result = true;
      }
  
-     return entrance;
+     return result;
    }
-};
+ };
 
-console.log(deepСheck(obj1)); 
-console.log(deepСheck(obj2));
-console.log(deepСheck(obj3));
+ console.log(isEmptyDeep(obj1)); 
+ console.log(isEmptyDeep(obj2));
+ console.log(isEmptyDeep(obj3));
 
 
 
